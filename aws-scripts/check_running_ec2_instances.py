37e64d28138118c40dc73aa1f6f8a8bb7a2dc4cb
akowal <akowal@sslaptop.local>
2023-03-03 13:37:31 +0100

    adding nat gateways function

--- aws-scripts/check_running_ec2_instances.py
+++ aws-scripts/check_running_ec2_instances.py
@@ -15,44 +15,100 @@ import boto3
 
 # function showing all ec2 instances
 def check_ec2_states_all_regions():
+    # initialize ec2 client
+    ec2 = boto3.client("ec2")
 
-  # initialize ec2 client 
-  ec2 = boto3.client('ec2')
+    regions = [region["RegionName"] for region in ec2.describe_regions()["Regions"]]
 
-  regions = [region['RegionName'] for region in ec2.describe_regions()['Regions']]
+    # iterate over each region
+    for region in regions:
+        print("Instances in region", region)
 
-  # iterate over each region
-  for region in regions:
-      print("Instances in region", region)
+        # initialize the EC2 resources
+        ec2 = boto3.resource("ec2", region_name=region)
 
+        # get list of all instances in the region
+        instances = ec2.instances.all()
 
-      # initialize the EC2 resources 
-      ec2 = boto3.resource('ec2', region_name=region)
+        # print instances
+        for i in instances:
+            print("Instance ID:", i.id)
+            print("State:", i.state["Name"])
 
-      # get list of all instances in the region 
-      instances = ec2.instances.all()
+def s3_check():
+    # using AWS S3
+    s3 = boto3.resource('s3')
+    
+    for bucket in s3.buckets.all():
+        print(bucket.name)
 
-      # print instances 
-      for i in instances:
-          print("Instance ID:", i.id)
-          print('State:', i.state['Name'])
+def check_nat_gateways():
+    # Create an EC2 client
+    ec2 = boto3.client('ec2')
+
+    # Retrieve information about all NAT gateways in the account
+    nat_gateways = ec2.describe_nat_gateways()
+
+    # Create an empty list to hold the IDs of all enabled NAT gateways
+    enabled_nat_gateway_ids = []
+
+    # Iterate over the NAT gateways and check if each one is enabled
+    for nat_gateway in nat_gateways['NatGateways']:
+        nat_gateway_id = nat_gateway['NatGatewayId']
+        nat_gateway_state = nat_gateway['State']
+        nat_gateway_subnet_id = nat_gateway['SubnetId']
+
+        # Check if the NAT gateway is enabled
+        if nat_gateway_state == 'available':
+            print(f"NAT gateway {nat_gateway_id} in subnet {nat_gateway_subnet_id} is enabled.")
+            enabled_nat_gateway_ids.append(nat_gateway_id)
+        else:
+            print(f"NAT gateway {nat_gateway_id} in subnet {nat_gateway_subnet_id} is not enabled.")
+
+    return enabled_nat_gateway_ids
 
 
 def main():
     # Create an argument parser
     parser = argparse.ArgumentParser(description="Check AWS account services status")
 
-    # Add an optional argument to check NAT gateways
-    parser.add_argument('--ec2-instances', action='store_true', help='Check for EC2 instances across regions')
+    # Add an optional argument to check EC2 instances
+    parser.add_argument(
+        "--ec2-instances",
+        action="store_true",
+        help="Check for EC2 instances across regions",
+    )
+
+    # Add an optional argument to check S3 buckets
+    parser.add_argument(
+        "--s3-list",
+        action="store_true",
+        help="Check for S3 buckets",
+    )    
+    
+    # Check for NAT gateways
+    parser.add_argument(
+        "--nat-gateways",
+        action="store_true",
+        help="Check for NAT gateways",
+    )
+
+
 
     # Parse the command-line arguments
     args = parser.parse_args()
 
-    # Check if the --nat-gateway argument was provided
+    # Parse args
     if args.ec2_instances:
         print("Running EC2 instances:")
         check_ec2_states_all_regions()
+    elif args.s3_list:
+        print("List of s3 buckets:" )
+        s3_check()
+    elif args.nat_gateways:
+        print("Check for NAT gateways:" )
+        check_nat_gateways()
+                
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
